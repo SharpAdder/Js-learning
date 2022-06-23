33293acbbd8fc8e447e6af83fc11a54c190acbdc
Adina Adam <49348559+SharpAdder@users.noreply.github.com>
2022-06-24 02:17:22 +0300

    Create 1.consoleLogs.js

---
+++ 1.consoleLogs.js
@@ -0,0 +1,16 @@
+
+// BOM alert 
+alert("Loading...");
+
+//logging to console
+console.log("Code executed");
+
+
+//warning log
+console.warn("You have a warning");
+
+
+//error logging
+console.error("Code did not execute");
+
+
