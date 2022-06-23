3435cbb1ea07787e41f5a721c5cc295e756245ae
Adina Adam <49348559+SharpAdder@users.noreply.github.com>
2022-06-24 02:22:08 +0300

    Create 2.varDeclare.js

---
+++ 2.varDeclare.js
@@ -0,0 +1,29 @@
+//VARIABLE DECLARATION
+//var, let, const
+
+var myName; 
+var myName = "Adina"; 
+let secondName = "Doe";
+
+
+const age = 36;
+
+myName = "Jane";
+
+
+{
+  var fruits = "apples";
+}
+
+console.log(fruits); //apples
+
+
+
+
+{
+  let shoppingCart = "shoes";
+}
+
+console.log(shoppingCart); //error: undefined
+
+//HOISTING --> moving up late var declarations, not applicable to obj
